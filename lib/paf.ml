6954b79a105b911fc830545a9c890cf60991913f
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-02-04 16:35:06 +0100

    Fix some issues about underlying socket used by HTTP/AF

--- lib/paf.ml
+++ lib/paf.ml
@@ -18,11 +18,15 @@ module Httpaf
     (Flow : Tuyau_mirage.F with type flow = Service.flow)
   : HTTPAF with type flow = Flow.flow
 = struct
+  let src = Logs.Src.create "paf"
+  module Log = (val Logs.src_log src : Logs.LOG)
+
   type t =
     { flow : Flow.flow
     ; rd : Cstruct.t
     ; queue : (char, Bigarray.int8_unsigned_elt) Ke.t
-    ; mutable closed : bool }
+    ; mutable rd_closed : bool
+    ; mutable wr_closed : bool }
 
   type flow = Flow.flow
   type endpoint = Ipaddr.V4.t * int
@@ -40,16 +44,20 @@ module Httpaf
   let rec recv flow ~read ~read_eof =
     match Ke.N.peek flow.queue with
     | [] ->
-      let len = min (Ke.available flow.queue) (Cstruct.len flow.rd) in
-      let raw = Cstruct.sub flow.rd 0 len in
-      ( Flow.recv flow.flow raw >>= function
-          | Error err -> Lwt.fail (Recv_error err)
-          | Ok `End_of_input ->
-            let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in
-            flow.closed <- true ; Lwt.return ()
-          | Ok (`Input len) ->
-            let _ = Ke.N.push_exn flow.queue ~blit ~length:Cstruct.len ~off:0 ~len raw in
-            recv flow ~read ~read_eof )
+      if flow.rd_closed
+      then ( let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in Lwt.return () )
+      else
+        let len = min (Ke.available flow.queue) (Cstruct.len flow.rd) in
+        let raw = Cstruct.sub flow.rd 0 len in
+        ( Flow.recv flow.flow raw >>= function
+            | Error err -> Lwt.fail (Recv_error err)
+            | Ok `End_of_input ->
+              let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in
+              flow.rd_closed <- true ; Lwt.return ()
+            | Ok (`Input len) ->
+              Log.debug (fun m -> m "<- %S" (Cstruct.to_string (Cstruct.sub raw 0 len))) ;
+              let _ = Ke.N.push_exn flow.queue ~blit ~length:Cstruct.len ~off:0 ~len raw in
+              recv flow ~read ~read_eof )
     | src :: _ ->
       let len = Bigstringaf.length src in
       let shift = read src ~off:0 ~len in
@@ -57,70 +65,97 @@ module Httpaf
       Lwt.return ()
 
   let send flow iovecs =
-    if flow.closed then Lwt.return `Closed
+    if flow.wr_closed then Lwt.return `Closed
     else
       let rec go w = function
         | [] -> Lwt.return (`Ok w)
         | { Faraday.buffer; Faraday.off; Faraday.len; } :: rest ->
           let raw = Cstruct.of_bigarray buffer ~off ~len in
+          Log.debug (fun m -> m "-> %S" (Cstruct.to_string raw)) ;
           Flow.send flow.flow raw >>= function
           | Ok ws ->
             if ws = len then go (w + ws) rest
             else Lwt.return (`Ok (w + ws))
-          | Error err -> Lwt.fail (Send_error err) in
+          | Error err ->
+            (* TODO(dinosaure): [Socket_closed]. *)
+            flow.wr_closed <- true ;
+            Lwt.fail (Send_error err) in
       go 0 iovecs
 
   let close flow =
-    if flow.closed
+    if flow.rd_closed && flow.wr_closed
     then Lwt.return ()
-    else ( flow.closed <- true
+    else ( flow.rd_closed <- true
+         ; flow.wr_closed <- true
          ; Flow.close flow.flow >>= function
            | Ok () -> Lwt.return ()
            | Error err -> Lwt.fail (Close_error err) )
 
   let create_connection_handler ?(config= Httpaf.Config.default) ~request_handler ~error_handler =
     let connection_handler (edn : endpoint) flow =
+      let ip, port = edn in
+      Log.debug (fun m -> m "new server connection <%a:%d>." Ipaddr.V4.pp ip port) ;
       let module Server_connection = Httpaf.Server_connection in
       let connection =
         Server_connection.create ~config ~error_handler:(error_handler edn)
           (request_handler edn) in
       let queue, _ = Ke.create ~capacity:0x1000 Bigarray.Char in
       let flow =
-        { flow; rd= Cstruct.create config.read_buffer_size; queue; closed= false; } in
+        { flow; rd= Cstruct.create config.read_buffer_size; queue; rd_closed= false; wr_closed= false; } in
       let rd_exit, notify_rd_exit = Lwt.wait () in
       let rec rd_fiber () =
-        let go () = match Server_connection.next_read_operation connection with
+        let rec go () = match Server_connection.next_read_operation connection with
           | `Read ->
+            Log.debug (fun m -> m "[`read]") ;
             recv flow
               ~read:(Server_connection.read connection)
-              ~read_eof:(Server_connection.read_eof connection)
+              ~read_eof:(Server_connection.read_eof connection) >>= fun () ->
+            Log.debug (fun m -> m "[`read] is done.") ; go ()
           | `Yield ->
+            Log.debug (fun m -> m "[read:`yield]") ;
             Server_connection.yield_reader connection rd_fiber ;
             Lwt.return ()
           | `Close ->
+            Log.debug (fun m -> m "[read:`close]") ;
             Lwt.wakeup_later notify_rd_exit () ;
-            close flow in
+            (* XXX(dinosaure): HTTP/AF relies on one specific aspect of UNIX socket:
+               the ability to close as soon as possible the read-side regardless the write
+               side. However, the TCP socket provided by [mirage-tcpip] or interface provided
+               by [tuyau.mirage] does not give to us this aspect.
+
+               So we fake it by two boolean and ensure to _really_ close the socket at the
+               end of the process.
+            
+               See [httpaf-lwt-unix] for more details. *)
+            flow.rd_closed <- true ;
+            Lwt.return () in
         Lwt.async @@ fun () ->
         Lwt.catch go (fun exn -> Server_connection.report_exn connection exn ; Lwt.return ()) in
       let wr_exit, notify_wr_exit = Lwt.wait () in
       let rec wr_fiber () =
         let rec go () = match Server_connection.next_write_operation connection with
           | `Write iovecs ->
+            Log.debug (fun m -> m "[`write]");
             send flow iovecs >>= fun len ->
             Server_connection.report_write_result connection len ;
             go ()
           | `Yield ->
+            Log.debug (fun m -> m "[write:`yield]") ;
             Server_connection.yield_writer connection wr_fiber ;
             Lwt.return ()
           | `Close _ ->
+            Log.debug (fun m -> m "[write:`close]") ;
             Lwt.wakeup_later notify_wr_exit () ;
-            close flow in
+            flow.wr_closed <- true ;
+            Lwt.return () in
         Lwt.async @@ fun () ->
         Lwt.catch go (fun exn -> Server_connection.report_exn connection exn ; Lwt.return ()) in
       rd_fiber () ;
       wr_fiber () ;
       Lwt.join [ rd_exit; wr_exit ] >>= fun () ->
-      if flow.closed
+      let ip, port = edn in
+      Log.debug (fun m -> m "close <%a:%d>." Ipaddr.V4.pp ip port) ;
+      if not flow.rd_closed || not flow.wr_closed
       then close flow
       else Lwt.return () in
     connection_handler
@@ -149,7 +184,7 @@ module Make (StackV4 : Mirage_stack.V4) = struct
       | Error err -> Lwt.return (Rresult.R.error_msgf "%a" Service.pp_error err)
       | Ok flow ->
         let edn = TCP.dst flow in
-        Lwt.async (fun () -> handler edn flow) ; Lwt.pause () >>= go in
+        handler edn flow >>= go in
     go ()
 
   let https ?config ~error_handler ~request_handler master =
