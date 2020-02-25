e1d5736003990be05c0bdb9de8770a1bc38ccade
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-02-25 13:39:01 +0100

    Protect against dead-threads

--- lib/paf.ml
+++ lib/paf.ml
@@ -26,7 +26,8 @@ module Httpaf
     ; rd : Cstruct.t
     ; queue : (char, Bigarray.int8_unsigned_elt) Ke.t
     ; mutable rd_closed : bool
-    ; mutable wr_closed : bool }
+    ; mutable wr_closed : bool
+    ; mutable closed : bool }
 
   type flow = Flow.flow
   type endpoint = Ipaddr.V4.t * int
@@ -45,7 +46,7 @@ module Httpaf
     match Ke.N.peek flow.queue with
     | [] ->
       if flow.rd_closed
-      then ( let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in Lwt.return () )
+      then ( let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in Lwt.return `Closed )
       else
         let len = min (Ke.available flow.queue) (Cstruct.len flow.rd) in
         let raw = Cstruct.sub flow.rd 0 len in
@@ -53,7 +54,8 @@ module Httpaf
             | Error err -> Lwt.fail (Recv_error err)
             | Ok `End_of_input ->
               let _ (* 0 *) = read_eof Bigstringaf.empty ~off:0 ~len:0 in
-              flow.rd_closed <- true ; Lwt.return ()
+              Log.debug (fun m -> m "[`read] Connection closed.") ;
+              flow.rd_closed <- true ; Lwt.return `Closed
             | Ok (`Input len) ->
               Log.debug (fun m -> m "<- %S" (Cstruct.to_string (Cstruct.sub raw 0 len))) ;
               let _ = Ke.N.push_exn flow.queue ~blit ~length:Cstruct.len ~off:0 ~len raw in
@@ -61,8 +63,9 @@ module Httpaf
     | src :: _ ->
       let len = Bigstringaf.length src in
       let shift = read src ~off:0 ~len in
+      Log.debug (fun m -> m "[`read] shift %d/%d byte(s)" shift len) ;
       Ke.N.shift_exn flow.queue shift ;
-      Lwt.return ()
+      Lwt.return `Continue
 
   let send flow iovecs =
     if flow.wr_closed then Lwt.return `Closed
@@ -84,10 +87,11 @@ module Httpaf
       go 0 iovecs
 
   let close flow =
-    if flow.rd_closed && flow.wr_closed
+    if flow.closed
     then Lwt.return ()
     else ( flow.rd_closed <- true
          ; flow.wr_closed <- true
+         ; flow.closed <- true
          ; Flow.close flow.flow >>= function
            | Ok () -> Lwt.return ()
            | Error err -> Lwt.fail (Close_error err) )
@@ -102,16 +106,23 @@ module Httpaf
           (request_handler edn) in
       let queue, _ = Ke.create ~capacity:0x1000 Bigarray.Char in
       let flow =
-        { flow; rd= Cstruct.create config.read_buffer_size; queue; rd_closed= false; wr_closed= false; } in
-      let rd_exit, notify_rd_exit = Lwt.wait () in
+        { flow; rd= Cstruct.create config.read_buffer_size; queue; rd_closed= false; wr_closed= false; closed= false; } in
+      let rd_exit, notify_rd_exit = Lwt.task () in
+      let wr_exit, notify_wr_exit = Lwt.task () in
       let rec rd_fiber () =
         let rec go () = match Server_connection.next_read_operation connection with
           | `Read ->
             Log.debug (fun m -> m "[`read]") ;
-            recv flow
+            ( recv flow
               ~read:(Server_connection.read connection)
-              ~read_eof:(Server_connection.read_eof connection) >>= fun () ->
-            Log.debug (fun m -> m "[`read] is done.") ; go ()
+              ~read_eof:(Server_connection.read_eof connection) >>= function
+              | `Closed ->
+                Log.debug (fun m -> m "[`read] Connection closed (wake-up threads)") ;
+                Lwt.wakeup notify_rd_exit () ;
+                Lwt.wakeup notify_wr_exit () ;
+                flow.rd_closed <- true ;
+                Lwt.return ()
+              | `Continue -> Log.debug (fun m -> m "[`read] is done.") ; go () )
           | `Yield ->
             Log.debug (fun m -> m "[read:`yield]") ;
             Server_connection.yield_reader connection rd_fiber ;
@@ -131,8 +142,11 @@ module Httpaf
             flow.rd_closed <- true ;
             Lwt.return () in
         Lwt.async @@ fun () ->
-        Lwt.catch go (fun exn -> Server_connection.report_exn connection exn ; Lwt.return ()) in
-      let wr_exit, notify_wr_exit = Lwt.wait () in
+        Lwt.catch go (fun exn ->
+            Server_connection.report_exn connection exn ;
+            Log.warn (fun m -> m "[`read] got an exception: %s" (Printexc.to_string exn)) ;
+            if Lwt.state rd_exit = Sleep then Lwt.wakeup notify_rd_exit () ;
+            Lwt.return ()) in
       let rec wr_fiber () =
         let rec go () = match Server_connection.next_write_operation connection with
           | `Write iovecs ->
@@ -150,7 +164,11 @@ module Httpaf
             flow.wr_closed <- true ;
             Lwt.return () in
         Lwt.async @@ fun () ->
-        Lwt.catch go (fun exn -> Server_connection.report_exn connection exn ; Lwt.return ()) in
+        Lwt.catch go (fun exn ->
+            Server_connection.report_exn connection exn ;
+            Log.warn (fun m -> m "[`write] got an exception: %s" (Printexc.to_string exn)) ;
+            if Lwt.state wr_exit = Sleep then Lwt.wakeup notify_wr_exit () ;
+            Lwt.return ()) in
       rd_fiber () ;
       wr_fiber () ;
       let threads = [ rd_exit; wr_exit; ] in
@@ -160,12 +178,11 @@ module Httpaf
           List.iter Lwt.cancel threads ;
           !Lwt.async_exception_hook v in
       List.iter (fun th -> Lwt.on_failure th catch_and_cancel) threads ;
-      Lwt.join threads >>= fun () ->
       let ip, port = edn in
+      Log.debug (fun m -> m "<%a:%d> waiting." Ipaddr.V4.pp ip port) ;
+      Lwt.join threads >>= fun () ->
       Log.debug (fun m -> m "close <%a:%d>." Ipaddr.V4.pp ip port) ;
-      if not flow.rd_closed || not flow.wr_closed
-      then close flow
-      else Lwt.return () in
+      close flow in
     connection_handler
 end
 
