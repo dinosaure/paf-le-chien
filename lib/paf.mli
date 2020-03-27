0fbab9f15da81a94a651f3d3990e1d1ab42a12cf
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-03-27 15:51:36 +0100

    Expose protocol and service with TLS

--- lib/paf.mli
+++ lib/paf.mli
@@ -1,10 +1,14 @@
 open Tuyau_mirage
+open Tuyau_mirage_tls
 
 module Make (Time : Mirage_time.S) (StackV4 : Mirage_stack.V4) : sig
   module TCP : module type of Tuyau_mirage_tcp.Make(StackV4)
 
   val tls_endpoint : (TCP.endpoint * Tls.Config.client) key
   val tls_configuration : (TCP.configuration * Tls.Config.server) key
+
+  val tls_protocol : TCP.protocol protocol_with_tls Witness.protocol
+  val tls_service : (TCP.service service_with_tls * TCP.protocol protocol_with_tls) Witness.service
 
   val http
     :  ?config:Httpaf.Config.t
