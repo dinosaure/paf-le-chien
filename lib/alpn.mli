46f961c10eec0a3c5154813974cf6f9e8ab9ecc1
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-04-09 17:53:23 +0200

    Add documentation on ALPN module

--- lib/alpn.mli
+++ lib/alpn.mli
@@ -1,3 +1,16 @@
+(** ALPN support.
+
+    [Alpn] depend on [http/af] & [h2] and choose them because they share the
+    same {!Paf.RUNTIME} interface. [Alpn] does not require [ocaml-tls] so it's
+    possible to use OpenSSL. It requires, at least:
+
+    - Something to extract ALPN result from the TLS {i flow}
+    - Something to represent as the string the peer (useful for over-framework)
+    - An injection function (available from [mimic])
+
+    In other words, [Alpn] did the only choice to trust on [http/af] & [h2] to
+    handle HTTP/1.0, HTTP/1.1 and H2 protocols. *)
+
 type ('reqd, 'hdr, 'req, 'resp, 'c, 'a) protocol =
   | HTTP_1_0
       : ( Httpaf.Reqd.t,
@@ -22,7 +35,7 @@ type ('reqd, 'hdr, 'req, 'resp, 'c, 'a) protocol =
           H2.Response.t,
           'c,
           'c H2.Body.t )
-        protocol
+        protocol  (** Type of protocols. *)
 
 type 'c body = Body : (_, _, _, _, 'c, 'v) protocol * 'v -> 'c body
 
@@ -46,6 +59,27 @@ type 'flow info = {
   peer : 'flow -> string;
   injection : 'flow -> Mimic.flow;
 }
+(** The type of information from a ['flow]:
+
+    - [alpn] is a fonction which is able to extract the result of the
+      negotiation between the client & the server about which protocol we need
+      to start
+    - [peer] returns a [string] representation of the given ['flow] to help to
+      print out some logs about this client.
+    - [injection] is the function which wraps the given ['flow] to a
+      [Mimic.flow].
+
+    For the last function, it can be done if you already registered the protocol
+    with [mimic]. In that case, the second value given by [Mimic.register] helps
+    you to {i inject} your flow as a [Mimic.flow]:
+
+    {[
+      let _, protocol = Mimic.register ~name:"my-protocol" (module My_protocol)
+
+      let injection (flow : My_protocol.flow) : Mimic.flow =
+        let module R = (val Mimic.repr protocol) in
+        R.T flow
+    ]} *)
 
 val serve :
   'flow info ->
@@ -59,27 +93,18 @@ val serve :
   ('t -> ('flow, ([> `Closed | `Msg of string ] as 'error)) result Lwt.t) ->
   ('t -> unit Lwt.t) ->
   't Paf.service
+(** [serve info ~error_handler ~request_handler accept close] creates a new
+    {!Paf.service} over the {i socket} ['t]. From the given implementation of
+    [accept] and [close], we are able to instantiate the {i main loop}. Then,
+    from the given [info], we extract informations such the application layer
+    protocol and choose which protocol we will use. Currently, if [info.alpn]
+    returns:
 
-val response_handler_v1_0 :
-  'edn ->
-  ('edn -> [ `read ] resp_handler -> unit) ->
-  Httpaf.Response.t ->
-  [ `read ] Httpaf.Body.t ->
-  unit
+    - [Some "http/1.0" | Some "http/1.1" | None], we launch an [http/af] service
+    - [Some "h2"], we launch an [h2] service
 
-val response_handler_v1_1 :
-  'edn ->
-  ('edn -> [ `read ] resp_handler -> unit) ->
-  Httpaf.Response.t ->
-  [ `read ] Httpaf.Body.t ->
-  unit
-
-val response_handler_v2_0 :
-  'edn ->
-  ('edn -> [ `read ] resp_handler -> unit) ->
-  H2.Response.t ->
-  [ `read ] H2.Body.t ->
-  unit
+    The user is able to identify which protocol we launched by {!resd_handler}.
+    The returned service can be run with {!Paf.serve}. *)
 
 type client_error =
   [ `Exn of exn
@@ -87,15 +112,6 @@ type client_error =
   | `Invalid_response_body_length_v1 of Httpaf.Response.t
   | `Invalid_response_body_length_v2 of H2.Response.t
   | `Protocol_error of H2.Error_code.t * string ]
-
-val error_handler_v1 :
-  'edn ->
-  ('edn -> client_error -> unit) ->
-  Httpaf.Client_connection.error ->
-  unit
-
-val error_handler_v2 :
-  'edn -> ('edn -> client_error -> unit) -> H2.Client_connection.error -> unit
 
 val run :
   sleep:Paf.sleep ->
@@ -106,3 +122,7 @@ val run :
   [ `V1 of Httpaf.Request.t | `V2 of H2.Request.t ] ->
   Mimic.flow ->
   ([ `write ] body, [> `Msg of string ]) result Lwt.t
+(** [run ~sleep ?alpn ~error_handler ~response_handler edn req flow] tries
+    communitate to [edn] via [flow] with a certain protocol according to the
+    given [alpn] value and the given request. It returns the body of the request
+    to allow the user to write on it (and communicate then with the server). *)
