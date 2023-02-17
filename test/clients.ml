86fd6cff68673b2f781e60b7c21cd6d56db30894
Hannes Mehnert <hannes@mehnert.org>
2023-02-17 13:22:51 +0100

    dune build @fmt --auto and upgrade to ocamlformat 0.24.1

--- test/clients.ml
+++ test/clients.ml
@@ -28,7 +28,9 @@ let run uri =
 
 let run_client uri =
   run_process (fun () ->
-      let () = Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna) in
+      let () =
+        Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna)
+      in
       Lwt_main.run (run uri))
 
 let const x _ = x
